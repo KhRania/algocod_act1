a3f679a8c3b2c48edc0942870b19770ed6642e23
KhRania <rkhadaier@gmail.com>
2020-02-03 16:11:59 +0100

    add activity 2 in the repository

---
+++ act2/build/patrol/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/rania/act2/src/patrol
+# Build directory: /home/rania/act2/build/patrol
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
